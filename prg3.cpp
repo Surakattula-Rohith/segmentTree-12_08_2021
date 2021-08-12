046388ec63444d4fbe249c6ea33d3ca8e79066d3
Surakattula Rohith <74316672+Surakattula-Rohith@users.noreply.github.com>
2021-08-12 11:54:20 +0530

    Add files via upload

---
+++ prg3.cpp
@@ -0,0 +1,195 @@
+#include <bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define nl "\n"
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp make_pair
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define all(n) n.begin(), n.end()
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define bitc(n) __builtin_popcount(n)
+#define loop(i, a, b) for (int i = (a); i <= (b); i++)
+#define looprev(i, a, b) for (int i = (a); i >= (b); i--)
+#define iter(container, it) for (__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...)                             \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+#define logarr(arr, a, b)            \
+    for (int z = (a); z <= (b); z++) \
+        cout << (arr[z]) << " ";     \
+    cout << endl;
+template <typename T>
+T gcd(T a, T b)
+{
+    if (a % b)
+        return gcd(b, a % b);
+    return b;
+}
+template <typename T>
+T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+vs tokenizer(string str, char ch)
+{
+    std::istringstream var((str));
+    vs v;
+    string t;
+    while (getline((var), t, (ch)))
+    {
+        v.pb(t);
+    }
+    return v;
+}
+
+void err(istream_iterator<string> it) {}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cout << *it << " = " << a << endl;
+    err(++it, args...);
+}
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void update1(int *arr, int *tree, int s, int e, int tidx, int idx)
+{
+    if (s == e)
+    {
+        tree[tidx]++;
+        arr[idx] = 2 * arr[idx] + 1;
+        return;
+    }
+    int mid = (s + e) / 2;
+    if (idx < mid)
+    {
+        update1(arr, tree, s, mid, 2 * tidx, idx);
+    }
+    else
+    {
+        update1(arr, tree, mid + 1, e, 2 * tidx + 1, idx);
+    }
+
+    tree[tidx] = tree[2 * tidx] + tree[2 * tidx + 1];
+}
+
+void update2(int *arr, int *tree, int s, int e, int tidx, int idx)
+{
+    if (s == e)
+    {
+        if (tree[tidx] > 0)
+            tree[tidx]--;
+        arr[idx] = arr[idx] / 2;
+        return;
+    }
+    int mid = (s + e) / 2;
+    if (idx < mid)
+    {
+        update2(arr, tree, s, mid, 2 * tidx, idx);
+    }
+    else
+    {
+        update2(arr, tree, mid + 1, e, 2 * tidx + 1, idx);
+    }
+
+    tree[tidx] = tree[2 * tidx] + tree[2 * tidx + 1];
+}
+
+int query(int *tree, int s, int e, int tidx, int l, int r)
+{
+    if (s > r or e < l)
+    {
+        //completly outside
+        return 0;
+    }
+    if (s >= l and e <= r)
+    {
+        //completly inside
+        return tree[tidx];
+    }
+    //partially inside
+    int mid = (s + e) / 2;
+    int left = query(tree, s, mid, 2 * tidx, l, r);
+    int right = query(tree, mid + 1, e, 2 * tidx + 1, l, r);
+
+    return (left + right);
+}
+
+int main(int argc, char const *argv[])
+{
+    clock_t begin = clock();
+    file_i_o();
+    int n;
+    cin >> n;
+    int arr[n];
+    int segtree[4 * n];
+    loop(i, 0, 4 * n - 1)
+    {
+        segtree[i] = 0;
+    }
+
+    loop(i, 0, n - 1)
+    {
+        arr[i] = 0;
+    }
+
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int type;
+        cin >> type;
+        if (type == 1)
+        {
+            int l, r;
+            cin >> l >> r;
+            int ans = query(segtree, 0, n - 1, 1, l, r);
+            cout << ans << nl;
+        }
+        else if (type == 2)
+        {
+            int i;
+            cin >> i;
+            update1(arr, segtree, 0, n - 1, 1, i - 1);
+        }
+        else
+        {
+            int i;
+            cin >> i;
+            update2(arr, segtree, 0, n - 1, 1, i - 1);
+        }
+    }
+
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted In: " << double(end - begin) / CLOCKS_PER_SEC * 1000 << " ms";
+#endif
+    return 0;
+}
